7511e961eece6d8724451d0797a4810c2621c1ca
ricardo lopez magaña <ricardo.lm@globant.com>
2022-09-20 19:56:39 -0500

    create new commit

---
+++ enjoy-holidays-app/enjoy-holidays-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  enjoy-holidays-app
+//
+//  Created by Ricardo Lopez Magana on 20/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
